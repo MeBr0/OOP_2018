7e2c23aed2e14ebd02b66ee52178c669a617f0ed
mebr0 <aza17e@gmail.com>
2018-09-04 12:40:20 +0600

    first commit

---
+++ week2/IO/MKAD.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MKAD{
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        a = (a + 109) % 109;
+
+        System.out.println((a * b) % 109);
+    }
+}
---
+++ week2/While/min_divisor.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class min_divisor{
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        //        int b = scan.nextInt();
+        //        int c = scan.nextInt();
+        //        int d = scan.nextInt();
+
+        int i = 2;
+
+        while (i <= a){
+            if (a % i == 0){
+                System.out.println(i);
+                break;
+            }
+
+            i++;
+        }
+
+
+    }
+}
