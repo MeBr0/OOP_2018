cbb134b668b20ee75739078350bbea371e0ab9ec
mebr0 <aza17e@gmail.com>
2018-09-11 00:58:09 +0600

    added pr6

--- week 3/pr6.java
+++ week 3/pr6.java
@@ -1,7 +1,65 @@
 import java.util.Scanner;
 
-public class pr6{
-    private static String convertDigit(int n){
+class Converter{
+    public String inWords(int n){
+        String res = "";
+
+        if (n == 0){
+            res = "zero";
+        }
+
+        else{
+            int first = n / 1000;
+            int second = n % 1000;
+
+            if (first != 0) {
+                res += this.convertThreeDigits(first) + " thousand";
+
+                if (first % 10 != 1){
+                    res += "s";
+                }
+
+                res += " ";
+            }
+            if (second != 0) {
+                res += this.convertThreeDigits(second);
+            }
+        }
+        return res;
+    }
+
+    private String convertThreeDigits(int n) {
+        String res = "";
+
+        int a = n / 100;
+        int b = n / 10 % 10;
+        int c = n % 10;
+
+        if (a != 0) {
+            res += this.convertDigit(a) + " hundred ";
+
+            if (b != 0 || c != 0){
+                res += "and ";
+            }
+        }
+
+        if (b == 1) {
+            res += this.convertSecondDigits(c) + " ";
+        }
+        else {
+            if (b != 0) {
+                res += this.convertSecondDigit(b) + " ";
+            }
+
+            if (c != 0) {
+                res += this.convertDigit(c);
+            }
+        }
+
+        return res;
+    }
+
+    private String convertDigit(int n){
         String res = "";
 
         switch (n){
@@ -33,35 +91,90 @@ public class pr6{
                 res = "nine";
                 break;
         }
+        return res;
+    }
+
+    private String convertSecondDigit(int n){
+        String res = "";
+
+        switch (n){
+            case 2:
+                res = "twenty";
+                break;
+            case 3:
+                res = "thirty";
+                break;
+            case 4:
+                res = "fourty";
+                break;
+            case 5:
+                res = "fifty";
+                break;
+            case 6:
+                res = "sixty";
+                break;
+            case 7:
+                res = "seventy";
+                break;
+            case 8:
+                res = "eighty";
+                break;
+            case 9:
+                res = "ninety";
+                break;
+        }
 
         return res;
     }
 
-    public static String inWords(int number){
-        int[] digits = new int[5];
-
+    private String convertSecondDigits(int n){
         String res = "";
 
-        for (int i = 0; i < digits.length; ++i){
-            digits[i] = number % 10;
-
-            System.out.print(digits[i]);
-
-            number /= 10;
+        switch (n){
+            case 0:
+                res = "ten";
+                break;
+            case 1:
+                res = "eleven";
+                break;
+            case 2:
+                res = "twelve";
+                break;
+            case 3:
+                res = "thirteen";
+                break;
+            case 4:
+                res = "fourteen";
+                break;
+            case 5:
+                res = "fifteen";
+                break;
+            case 6:
+                res = "sixteen";
+                break;
+            case 7:
+                res = "seventeen";
+                break;
+            case 8:
+                res = "eighteen";
+                break;
+            case 9:
+                res = "nineteen";
+                break;
         }
 
-        if (digits[4] != 0){
-            res +=
-        }
+        return res;
     }
+}
 
+public class pr6{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
-        RuleBasedNumberFormat nf = new RuleBasedNumberFormat(Locale.forLanguageTag("en"));
-
         int a = scan.nextInt();
 
-        inWords(a);
+        Converter conv = new Converter();
+
+        System.out.println(conv.inWords(a));
     }
 }
